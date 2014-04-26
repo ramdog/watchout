96de88cbbdcd9baab0800fca5b16e32a9c30cf1f
Hack Reactor Students <talegram@gmail.com>
2014-04-25 21:30:56 -0700

    Makes a differently-colored dot to represent the player. Makes it draggable.

--- watchout.js
+++ watchout.js
@@ -25,16 +25,66 @@ var axes = {
 var createEnemies = function() {
   var enemies = [];
   for (var i = 0; i < gameOptions.nEnemies; i++) {
-    enemies.push({id: i, x: Math.random()*100, y: Math.random()*100});
+    enemies.push({
+      id: i,
+      x: Math.random()*100,
+      y: Math.random()*100
+    });
   }
 
   return enemies;
 };
 
-var enemies = createEnemies();
+var enemies = createEnemies(); // spits out an array of 30 objects enemies that does nothing.
+
+var Player = function() {
+  this.id = 1;
+  this.x = axes.x(50);
+  this.y = axes.y(50);
+  this.angle = 0;
+  this.r = 5;
+  this.path = "m-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z";
+  this.fill = "#ff6600";
+};
+
+var player = new Player();
+
+var dragmove = function () {
+  var dx = d3.event.dx;
+  var dy = d3.event.dy;
+  var x = Math.min(gameOptions.width-15, Math.max(d3.event.x, 15));
+  var y = Math.min(gameOptions.height-15, Math.max(d3.event.y, 15));
+  var angle = 360 * (Math.atan2(dy, dx) / (Math.PI * 2));
+  d3.select(this).attr("transform", function(){
+      var rotate = "rotate(" + angle + "," + x + "," + y + ") ";
+      var translate = "translate(" + x + "," + y + ")";
+
+      return rotate + translate;
+    });
+};
+
+var drag = d3.behavior.drag()
+    .on("drag", dragmove);
 
 
+var updatePlayer = function (data){
 
+  var player = gameBoard.selectAll("path")
+      .data(data, function(d){ return d.id; });
+
+  player.enter().append("svg:path")
+    .attr("class", "player")
+    .attr("d", function(d){return d.path;})
+    .attr("fill", function(d){return d.fill;})
+    .attr("transform", function(d){
+      var rotate = "rotate(0," + d.x + "," + d.y + ") ";
+      var translate = "translate(" + d.x + "," + d.y + ")";
+
+      return rotate + translate;
+    })
+    .style("cursor", "pointer")
+    .call(drag);
+};
 
 
 var updateEnemies = function (data) {
@@ -47,9 +97,9 @@ var updateEnemies = function (data) {
   // UPDATE
   // Update old elements as needed.
   enemies.transition()
-      .duration(750)
+      .duration(1000)
       .attr("cx", function(d, i) { return axes.x(d.x); })
-      .attr("cy", function(d, i) { return axes.y(d.y); })
+      .attr("cy", function(d, i) { return axes.y(d.y); });
 
 
   // ENTER
@@ -60,7 +110,7 @@ var updateEnemies = function (data) {
       .attr("cy", function(d, i) { return axes.y(d.y); })
       .attr("r", 10)
       .transition()
-      .duration(750);
+      .duration(1000);
 
   // ENTER + UPDATE
   // Appending to the enter selection expands the update selection to include
@@ -72,6 +122,8 @@ var updateEnemies = function (data) {
   // Remove old elements as needed.
   // text.exit().remove();
 };
+
+updatePlayer([player]);
 
 // The initial display.
 updateEnemies(enemies);
